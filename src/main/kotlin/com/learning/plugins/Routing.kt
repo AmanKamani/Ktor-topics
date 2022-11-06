c28f08e92a50ef27490f5347f02c619cdb2e7dce
AmanKamani <kamaniamanak@gmail.com>
2022-11-06 22:32:26 +0530

    Added CallLogging & GSON plugin.
    CallLogging: Logs each request sent to the server.
    GSON: Allows to serialize the object to json.

--- src/main/kotlin/com/learning/plugins/Routing.kt
+++ src/main/kotlin/com/learning/plugins/Routing.kt
@@ -1,13 +1,37 @@
 package com.learning.plugins
 
+import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.slf4j.event.Level
 
 fun Application.configureRouting() {
+
+    install(CallLogging) {
+        level = Level.INFO
+    }
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    // This installs the routing plugin with
+    // install(Routing)
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+        get("/person") {
+            val person = Person("ABC", 20)
+            call.respond(person)
+        }
     }
 }
+
+data class Person(
+    val name: String,
+    val age: Int
+)
